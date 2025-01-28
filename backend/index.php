94a7f843c0f4f00647baf2c460b0a3d17a16083f
xxxhqzm <xxxhqzm@gmail.com>
2025-01-28 22:59:26 +0800

    add backend structure

---
+++ backend/index.php
@@ -0,0 +1,14 @@
+<?php
+    // Getting data number of people/player ($_get)
+
+    // Data validation
+
+    // List of cards (names)
+
+    // Shuffle
+
+    // Fill the card into array based on the number of people/player
+
+    // Generate the output
+
+?>
