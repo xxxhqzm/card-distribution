985193be374f58de437f29e15e706fb348167aa2
xxxhqzm <xxxhqzm@gmail.com>
2025-01-29 10:57:01 +0800

    backend: add php code for the card distribution

--- backend/index.php
+++ backend/index.php
@@ -1,14 +1,61 @@
 <?php
+
     // Getting data number of people/player ($_get)
+    if (isset($_GET['num_people']) && is_numeric($_GET['num_people'])) {
+        $num_people = (int)$_GET['num_people'];
 
-    // Data validation
+        // Data validation
+        // Greater than 53 are normal values and cards must be distributed to a number of people instead of having it as an error.
+        if ($num_people < 0 || $num_people == NULL) {
+            echo "Input value does not exist or value is invalid";
+            exit;
+        }
 
-    // List of cards (names)
+        // List of cards (names)
+        $cards = array(
+            'S' => array('A', '2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K'),
+            'H' => array('A', '2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K'),
+            'D' => array('A', '2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K'),
+            'C' => array('A', '2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K')
+        );
 
-    // Shuffle
+        $deck = array();
+        foreach ($cards as $suit => $values) {
+            foreach ($values as $value) {
+                $deck[] = "$suit-$value";
+            }
+        }
 
-    // Fill the card into array based on the number of people/player
+        // Shuffle
+        shuffle($deck);
+        $distribution = array_fill(0, $num_people, []);
 
-    // Generate the output
+
+        // Fill the card into array based on the number of people/player
+        $card_index = 0;
+        while ($card_index < count($deck)) {
+            for ($person = 0; $person < $num_people; $person++) {
+                if ($card_index >= count($deck)) {
+                    break;
+                }
+                $distribution[$person][] = $deck[$card_index];
+                $card_index++;
+            }
+        }
+
+        // Generate the output
+        // Generate the output in the specified format
+
+        $output = [];
+        foreach ($distribution as $personCards) {
+            $output[] = implode(',', $personCards);
+        }
+
+        echo json_encode([
+            "status" => "success",
+            "message" => "Cards distributed successfully",
+            "data" => $output
+        ]);
+    }
 
 ?>
